8b50f919e0a0a4120fced256fc268da632d6154a
Kyle Redelinghuys <kyle@ksred.me>
2016-03-15 05:28:28 -0400

    Added delete account, account meta, full account delete

--- accounts/database.go
+++ accounts/database.go
@@ -54,6 +54,20 @@ func createAccount(accountDetails AccountDetails, accountHolderDetails AccountHo
 	return
 }
 
+func deleteAccount(accountDetails AccountDetails, accountHolderDetails AccountHolderDetails) (result bool, err error) {
+	err = doDeleteAccount(&accountDetails)
+	if err != nil {
+		return
+	}
+
+	err = doDeleteAccountMeta(&accountHolderDetails, &accountDetails)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
 func doCreateAccount(sqlTime int32, accountDetails *AccountDetails) (err error) {
 	db, err := sql.Open("mysql", Config.MySQLUser+":"+Config.MySQLPass+"@tcp("+Config.MySQLHost+":"+Config.MySQLPort+")/"+Config.MySQLDB)
 	if err != nil {
@@ -81,6 +95,29 @@ func doCreateAccount(sqlTime int32, accountDetails *AccountDetails) (err error) 
 	return
 }
 
+func doDeleteAccount(accountDetails *AccountDetails) (err error) {
+	db, err := sql.Open("mysql", Config.MySQLUser+":"+Config.MySQLPass+"@tcp("+Config.MySQLHost+":"+Config.MySQLPort+")/"+Config.MySQLDB)
+	if err != nil {
+		return
+	}
+	// Create account
+	deleteStatement := "DELETE FROM accounts WHERE `accountNumber` = ? AND `bankNumber` = ? AND `accountHolderName` = ?) "
+	stmtDel, err := db.Prepare(deleteStatement)
+	if err != nil {
+		return
+	}
+
+	// Prepare statement for inserting data
+	defer stmtDel.Close() // Close the statement when we leave main() / the program terminates
+
+	_, err = stmtDel.Exec(accountDetails.AccountNumber, accountDetails.BankNumber, accountDetails.AccountHolderName)
+	if err != nil {
+		return
+	}
+	// Can use db.RowsAffected() to check
+	return
+}
+
 func doCreateAccountMeta(sqlTime int32, accountHolderDetails *AccountHolderDetails, accountDetails *AccountDetails) (err error) {
 	db, err := sql.Open("mysql", Config.MySQLUser+":"+Config.MySQLPass+"@tcp("+Config.MySQLHost+":"+Config.MySQLPort+")/"+Config.MySQLDB)
 	if err != nil {
@@ -99,6 +136,34 @@ func doCreateAccountMeta(sqlTime int32, accountHolderDetails *AccountHolderDetai
 
 	_, err = stmtIns.Exec(accountHolderDetails.AccountNumber, accountHolderDetails.BankNumber, accountHolderDetails.GivenName, accountHolderDetails.FamilyName, accountHolderDetails.DateOfBirth, accountHolderDetails.IdentificationNumber, accountHolderDetails.ContactNumber1, accountHolderDetails.ContactNumber2, accountHolderDetails.EmailAddress, accountHolderDetails.AddressLine1, accountHolderDetails.AddressLine2, accountHolderDetails.AddressLine3,
 		accountHolderDetails.PostalCode, sqlTime)
+
+	if err != nil {
+		return
+	}
+
+	defer db.Close()
+
+	return
+}
+
+func doDeleteAccountMeta(accountHolderDetails *AccountHolderDetails, accountDetails *AccountDetails) (err error) {
+	db, err := sql.Open("mysql", Config.MySQLUser+":"+Config.MySQLPass+"@tcp("+Config.MySQLHost+":"+Config.MySQLPort+")/"+Config.MySQLDB)
+	if err != nil {
+		return
+	}
+	// Create account meta
+	deleteStatement := "DELETE FROM accounts_meta WHERE `accountNumber` = ? AND `bankNumber` = ? AND `accountHolderGivenName` = ? AND `accountHolderFamilyName` = ? AND `accountHolderDateOfBirth` = ? AND `accountHolderIdentificationNumber` = ? AND `accountHolderContactNumber1` = ? AND `accountHolderContactNumber2` = ? AND `accountHolderEmailAddress` = ? AND `accountHolderAddressLine1` = ? AND `accountHolderAddressLine2` = ? AND `accountHolderAddressLine3` = ? AND `accountHolderPostalCode` = ?) "
+	deleteStatement += "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+	stmtDel, err := db.Prepare(deleteStatement)
+	if err != nil {
+		return
+	}
+	defer stmtDel.Close() // Close the statement when we leave main() / the program terminates
+
+	accountHolderDetails.AccountNumber = accountDetails.AccountNumber
+
+	_, err = stmtDel.Exec(accountHolderDetails.AccountNumber, accountHolderDetails.BankNumber, accountHolderDetails.GivenName, accountHolderDetails.FamilyName, accountHolderDetails.DateOfBirth, accountHolderDetails.IdentificationNumber, accountHolderDetails.ContactNumber1, accountHolderDetails.ContactNumber2, accountHolderDetails.EmailAddress, accountHolderDetails.AddressLine1, accountHolderDetails.AddressLine2, accountHolderDetails.AddressLine3,
+		accountHolderDetails.PostalCode)
 
 	if err != nil {
 		return
