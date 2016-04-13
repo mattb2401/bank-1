f8ee078a95492003c52d7bd29f59341efd2d86b3
Kyle Redelinghuys <kyle@ksred.me>
2016-04-13 06:57:38 +0200

    Updated SQL

--- travis/build.sh
+++ travis/build.sh
@@ -2,3 +2,11 @@
 
 # Copy config
 cp $HOME/gopath/src/github.com/ksred/bank/travis/config.json $HOME/gopath/src/github.com/ksred/bank/
+
+# Create database
+mysql -u root -p -e "create database bank;"
+mysql -u root -p bank < $HOME/gopath/src/github.com/ksred/bank/sql/0-create-trans-table.sql
+mysql -u root -p bank < $HOME/gopath/src/github.com/ksred/bank/sql/1-create-accounts-table.sql
+mysql -u root -p bank < $HOME/gopath/src/github.com/ksred/bank/sql/2-create-account-meta-table.sql
+mysql -u root -p bank < $HOME/gopath/src/github.com/ksred/bank/sql/3-create-bank-holding-account.sql
+mysql -u root -p bank < $HOME/gopath/src/github.com/ksred/bank/sql/4-create-account-auth-table.sql
